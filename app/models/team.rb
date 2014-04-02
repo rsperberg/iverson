b91049b2dc352179aaebd00f1373d4b38d77f9cb
Simon <simon.slater@gmail.com>
2014-04-02 19:49:35 -0400

    games change

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,4 @@ class Team < ActiveRecord::Base
   has_many :games
   belongs_to :school
   
-   def win?(home_score, opponent_score)
-   		status = (home_score > opponent_score) ?  true :  false
-   end
 end
