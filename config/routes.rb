7a6ed0b1ad9db6022b053ec1c80e6716c0818073
Simon <simon.slater@gmail.com>
2014-04-01 21:37:26 -0400

    add school route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Iverson::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/school' => 'school#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
