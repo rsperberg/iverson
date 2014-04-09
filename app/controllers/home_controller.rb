8a79b032c675c2f7693c9b1e76d5d3f84b408b24
Doug Helferich <dhelf17@gmail.com>
2014-04-08 23:48:11 -0400

    Added home controller and featured games view with basic queries. -dh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
 	def index
-		@game = Game.all
+		# @date = Date.yesterday
+		@date = "03/04/2014"
+		if Game.where(["date LIKE :tag", {:tag => @date}]).count > 100
+			@game =  Game.where(["date LIKE :tag", {:tag => @date}]).first(100) 
+		else
+			@game = Game.all.first(100)
+		end
 	end
 end
