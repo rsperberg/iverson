3492da83d9c33d72268d20980c394937f6a47893
Simon <simon.slater@gmail.com>
2014-04-09 15:14:29 -0400

    working on search

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,7 @@
 class School < ActiveRecord::Base
   has_many :teams
+
+  def self.search(search)
+  	a=find(:all, :conditions => ['name LIKE ? OR state like ? OR city like ?', "%#{search}%","%#{search}%","%#{search}%"])	
+  	end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ActiveRecord::Base
   has_many :games
   belongs_to :school
-  
+   def self.search(search)
+  	find(:all, :conditions => ['name LIKE ?', "%#{search}%"])	
+  	end
 end
