66a4dbc977a2e087a67b73d40f1703817ed1badd
Simon <simon.slater@gmail.com>
2014-04-09 22:20:26 -0400

    Simon removed

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,10 +1,7 @@
 class School < ActiveRecord::Base
   has_many :teams
 
-  def self.search(search)	
-  	#query = "SELECT * FROM schools WHERE (schools.name LIKE '%#{search}%' OR schools.state like '%#{search}%' OR schools.city like '%#{search}%')"
-  	#School.find_by_sql(query)
-  	  
+  def self.search(search)	 
   	self.includes(:teams).where("schools.name LIKE ? OR state LIKE ? OR city LIKE ? OR teams.name LIKE?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%").references(:teams)
   	end
 end
