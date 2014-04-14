f02b2c54ca40e4bd50caf19fe1e35f19bb4632fe
js108 <jschoech@gmail.com>
2014-04-13 21:47:32 -0400

    styles many

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
   School.delete_all
 
   Game.create(ko_game_id: 01, score: 235, is_home: false,
-                      summary: "The bookies got killed on the biggest blow out since the Super Colon Blow of 1976",
+                      summary: "The bookies got killed on the biggest...",
                      video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
                      date: "10/03/2013", team_id: 5, opponent_score: 0, opponent_id: 8 )
 
@@ -27,7 +27,47 @@
   Game.create(ko_game_id: 02, score: 78, is_home: false,
                       summary: "Throw it down big man",
                      video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
-                     date: "03/04/2014", team_id: 7, opponent_score: 20, opponent_id: 10 )					 
+                     date: "03/04/2014", team_id: 7, opponent_score: 20, opponent_id: 10 )		
+                     
+                     Game.create(ko_game_id: 101, score: 235, is_home: false,
+                                         summary: "The bookies got killed on the biggest...",
+                                        video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                        date: "10/03/2013", team_id: 5, opponent_score: 0, opponent_id: 8 )
+
+                     Game.create(ko_game_id: 1021, score: 5, is_home: true,
+                                         summary: "A real boring sandwhich with cheese",
+                                        video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                        date: "01/03/2014", team_id: 6, opponent_score: 5, opponent_id: 9 )
+
+                     Game.create(ko_game_id: 102, score: 2350, is_home: false,
+                                         summary: "A Classic Marve Albert back biter",
+                                        video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                        date: "03/03/2014", team_id: 7, opponent_score: 0, opponent_id: 10 )
+
+                     Game.create(ko_game_id: 102, score: 78, is_home: false,
+                                         summary: "Throw it down big man",
+                                        video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                        date: "03/04/2014", team_id: 7, opponent_score: 20, opponent_id: 10 )	
+                                        
+                                        Game.create(ko_game_id: 201, score: 235, is_home: false,
+                                                            summary: "The bookies got killed on the biggest...",
+                                                           video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                                           date: "10/03/2013", team_id: 5, opponent_score: 0, opponent_id: 8 )
+
+                                        Game.create(ko_game_id: 2021, score: 5, is_home: true,
+                                                            summary: "A real boring sandwhich with cheese",
+                                                           video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                                           date: "01/03/2014", team_id: 6, opponent_score: 5, opponent_id: 9 )
+
+                                        Game.create(ko_game_id: 202, score: 2350, is_home: false,
+                                                            summary: "A Classic Marve Albert back biter",
+                                                           video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                                           date: "03/03/2014", team_id: 7, opponent_score: 0, opponent_id: 10 )
+
+                                        Game.create(ko_game_id: 202, score: 78, is_home: false,
+                                                            summary: "Throw it down big man",
+                                                           video_url: "http://prod-m100.cdn.krossover.com/prod-basketball2-2014-1ef799b224cffc8d2337-1600.mp4",
+                                                           date: "03/04/2014", team_id: 7, opponent_score: 20, opponent_id: 10 )				 
 					 
   Team.create(id: 5, school_id: 10, ko_team_id: 123, name: "Pukesville Vomits")
   Team.create(id: 6, school_id: 20, ko_team_id: 234, name: "Dorktown Remedials")
