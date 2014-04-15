66f16ef6eecffa56a157f586b473609f335ad8aa
Amy Ruan <nyc.amy@gmail.com>
2014-04-15 11:40:13 -0400

    amended search to include lowercase

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,7 @@
 class School < ActiveRecord::Base
   has_many :teams
 
-  def self.search(search)	 
-  	self.includes(:teams).where("schools.name LIKE ? OR state LIKE ? OR city LIKE ? OR teams.name LIKE?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%").references(:teams)
+  def self.search(search)
+  	self.includes(:teams).where("LOWER(schools.name) LIKE ? OR LOWER(state) LIKE ? OR LOWER(city) LIKE ? OR LOWER(teams.name) LIKE?", "%#{search.downcase}%","%#{search.downcase}%","%#{search.downcase}%","%#{search.downcase}%").references(:teams)
   	end
 end
