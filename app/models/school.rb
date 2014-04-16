7fb0227c5af05e8c04382ec8b29298ad31f475f9
Simon <simon.slater@gmail.com>
2014-04-16 14:02:17 -0400

    fixed links

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,7 @@
 class School < ActiveRecord::Base
   has_many :teams
-
+  search = search 
   def self.search(search)	 
-  	self.includes(:teams).where("schools.name LIKE ? OR state LIKE ? OR city LIKE ? OR teams.name LIKE?", "%#{search}%","%#{search}%","%#{search}%","%#{search}%").references(:teams)
+  	self.includes(:teams).where("schools.name LIKE ? OR state LIKE ? OR city LIKE ? OR teams.name LIKE?", "%#{search || search.capitalize}%","%#{search || search.capitalize}%","%#{search || search.capitalize}%","%#{search || search.capitalize}%").references(:teams)
   	end
 end
